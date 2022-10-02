6663b0e1fd0e0137a78c5c4fd2342ff0da6b6f18
arcenioCorteis <arcenioCorteis@github.com>
2022-10-02 22:36:26 +0200

    create review

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,8 @@
-import imp
+from .forms import ReviewForm
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from .models import Movie as Movies
+from django.shortcuts import get_object_or_404, redirect
+from .models import Movie as Movies, Review
 
 # Listing Movies
 def home(request):
@@ -22,7 +22,8 @@ def home(request):
     
 def detail(request, movie_id):
     movie = get_object_or_404(Movies, pk=movie_id)
-    return render(request, 'detail.html', {'movie':movie})
+    reviews = Review.objects.filter(movie = movie)
+    return render(request, 'detail.html', {'movie':movie, 'reviews':reviews})
 
 def about(requet):
     return HttpResponse("<h1>About us</h1>")
@@ -31,3 +32,28 @@ def signup(request):
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email':email})
 
+
+def createreview(request, movie_id):
+    movie = get_object_or_404(Movies, pk=movie_id)
+    if request.method == 'GET':
+        return render(request, 'createreview.html', {'form':ReviewForm(), 'movie':movie})
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.movie = movie
+            newReview.save()
+            return redirect(
+                'detail',
+                newReview.movie.id
+            )
+            
+        except:
+            return render(
+                'createreview.html',
+                {
+                    'form':ReviewForm(),
+                    'error': 'bad data passed in'
+                }
+            )
--- news/models.py
+++ news/models.py
@@ -5,4 +5,7 @@ class News(models.Model):
     body = models.TextField()
     date = models.DateField()
     
+    def __str__(self):
+        return self.headline
+    
     
