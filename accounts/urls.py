09411464a2628256fac9cba3b3ddf677b37911cd
arcenioCorteis <arcenioCorteis@github.com>
2022-10-02 19:55:30 +0200

    register and login funtion

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signupaccount/', views.signupaccount, name='signupaccount')
+    path('signupaccount/', views.signupaccount, name='signupaccount'),
+    path('logout', views.logoutaccount, name='logoutaccount'),
+    path('login/', views.loginaccount, name='loginaccount'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserCreateForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
 
 def signupaccount(request):
     if request.method == 'GET':
-        return render(request, 'signupaccount.html', {'form':UserCreationForm})
+        return render(request, 'signupaccount.html', {'form':UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:    
@@ -22,10 +22,35 @@ def signupaccount(request):
                 login(request, user)
                 return redirect('home')
             except IntegrityError:
-                return render(request, 'signupaccount.html', {'form': UserCreationForm, 'error':'Username already taken. Choose new username.'})
+                return render(request, 'signupaccount.html', {'form': UserCreateForm, 'error':'Username already taken. Choose new username.'})
         else:
             return render(
                 request,
                 'signupaccount.html',
-                {'form':UserCreationForm, 'error':'Passwords do not match'}
+                {'form':UserCreateForm, 'error':'Passwords do not match'}
             )
+            
+def logoutaccount(request):
+    logout(request)
+    return redirect('home')
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', {'form':AuthenticationForm})
+    else:
+        user = authenticate(
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            return render(
+                request,
+                'loginaccount.html',
+                {
+                    'form': AuthenticationForm(),
+                    'error': 'Username and password do not math'
+                }
+            )
+        else:
+            login(request, user)
+            return redirect('home')
